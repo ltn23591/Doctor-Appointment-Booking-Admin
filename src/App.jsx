b52321fe9ffc25e8e5464dc561d9233ed3b18288
ltn23591 <ltn23591@gmail.com>
2026-03-11 00:55:26 +0700

    lam xong navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import Login from './pages/Login.jsx';
 import { AdminContext } from './context/AdminContext.jsx';
+import Navbar from './components/Navbar.jsx';
 const App = () => {
     const { aToken } = useContext(AdminContext);
 
     return aToken ? (
-        <div>
+        <div className="bg-[#f8f9fd]">
             <ToastContainer />
+            <Navbar />
         </div>
     ) : (
         <>
