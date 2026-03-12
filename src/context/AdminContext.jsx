1ed5a1093379943b1f69c733f488c102688e50a5
ltn23591 <ltn23591@gmail.com>
2026-03-12 18:01:22 +0700

    lam xong doctorslist

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { createContext, useState } from 'react';
+import { toast } from 'react-toastify';
 // eslint-disable-next-line react-refresh/only-export-components
 export const AdminContext = createContext();
 
@@ -6,10 +8,29 @@ const AdminContextProvider = (props) => {
     const [aToken, setAtoken] = useState(
         localStorage.getItem('aToken') ? localStorage.setItem('aToken') : '',
     );
+    const [doctors, setDoctors] = useState([]);
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const values = { aToken, setAtoken, backendUrl };
+    const getAllDoctors = async () => {
+        try {
+            const {data} = await axios.post(
+                backendUrl + '/api/admin/all-doctors',
+                {},
+                { headers: { aToken } },
+            );
+            if (data.success) {
+                setDoctors(data.doctors);
+                console.log('>>>>>Check doctor', data.doctors);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+            console.log(error);
+        }
+    };
 
+    const values = { aToken, setAtoken, backendUrl, getAllDoctors, doctors };
     return (
         <AdminContext.Provider value={values}>
             {props.children}
